463f0d534417ce7ed9841e57158d0b662bf40204
lekkalavinaykumar@gmail.com <India@1234567890>
2021-03-18 19:44:13 -0500

    initial push 1

--- src/main/scala/guru/learningjournal/spark/examples/HelloSparkSQL.scala
+++ src/main/scala/guru/learningjournal/spark/examples/HelloSparkSQL.scala
@@ -15,7 +15,7 @@ object HelloSparkSQL extends Serializable {
       .config("spark-streaming.stopGracefullyOnshutdwn", "true")
       .config("spark-sql.shuffle.partitions", 1)
       .getOrCreate()
-
+    logger.info("INFO")
     /*val surveyDF = spark.read
       .option("header", "true")
       .option("inferSchema", "true")
@@ -28,12 +28,40 @@ object HelloSparkSQL extends Serializable {
     logger.info(countDF.collect().mkString("->"))
     //scala.io.StdIn.readLine()
     spark.stop()*/
+    // kafkastreams
+
+    /*  val lineDf = spark.readStream
+        .format("socket")
+        .option("host", "localhost")
+        .option("port", "9092")
+        .load()*/
+    // tcp port by using net cat
+
+    // ---->  Steps to perform for streaming  apps---->>   //
+
+    // 1 Read data from streamSource - Input Data Frame
+    // 2 Transform the data - >output DataFrame
+    // 3 write output -sink
+
+    // socket source   only for leaning
+    //Rate Source only for testing
+    // File source
+    // Kafka source and file are commonly used in real time spark streaming projects
+
+
     val lineDf = spark.readStream
-      .format("socket")
+      .format("socket") //  here socket because the source is socket
       .option("host", "localhost")
-      .option("port", "9092")
+      .option("port", "9999")
       .load()
+
+    // on lineDf we can apply Most of the all the transfomations which we apply on spark batch transformations
+
+
+    // To verify the schema
     lineDf.printSchema()
+
+    // Applying the Transformation on the input datasets
     val wordDf = lineDf.select(expr("explode(split(value,' ')) as word"))
     val countDf = wordDf.groupBy("word").count()
     val query = countDf.writeStream.format("console")
--- src/main/scala/guru/learningjournal/spark/examples/KafkaStart.scala
+++ src/main/scala/guru/learningjournal/spark/examples/KafkaStart.scala
@@ -1,7 +1,46 @@
 package guru.learningjournal.spark.examples
 
+import guru.learningjournal.spark.examples.HelloSparkSQL.logger
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.expr
+
 object KafkaStart {
   def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("Hello Spark SQL")
+      .master("local[3]")
+      .config("spark-streaming.stopGracefullyOnshutdwn", "true")
+      .config("spark-sql.shuffle.partitions", 1)
+      .config("spark.sql.streaming.schemaInference", "true")
+      .config("spark.sql.streaming.checkpointLocation", "true")
+      .getOrCreate()
+    logger.info("INFO")
+
+    val linedf = spark.readStream.format("json")
+      .option("path", "input")
+      .load()
+    linedf.printSchema()
+
+    // Transformation on datasets
+    val frame = linedf.selectExpr("InvoiceNumber", "CreatedTime", "StoreId", "PosId", "CustomerType"
+      , "PaymentMethod", "DeliveryType", "DeliveryAddress.City", "DeliveryAddress.State", "DeliveryAddress.PinCode",
+      "explode(InvoiceLineItems) as LineItem")
+
+    val flattendDf = frame
+      .withColumn("ItemCode", expr("LineItem.ItemCode"))
+      .withColumn("ItemDescription", expr("LineItem.ItemDescription"))
+      .withColumn("Itemprice", expr("LineItem.Itemprice"))
+      .withColumn("ItemQty", expr("LineItem.ItemQty"))
+      .withColumn("TotalValue", expr("LineItem.TotalValue"))
+
+    val resu = flattendDf.writeStream
+      .format("json")
+      .option("path", "output")
+      .option("checkpointlocation", "chkpointdir")
+      .outputMode("append")
+      .queryName("flattend-invoice")
+      .start()
+
 
   }
 
